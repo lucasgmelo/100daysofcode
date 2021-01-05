a0a69ce201ac010236d9876c2bb63cedbee6e2bf
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-01-05 20:37:12 -0300

    feat(day-13): bg toggle done

--- day-13/app.js
+++ day-13/app.js
@@ -1,3 +1,4 @@
 function toggleMenu() {
-    document.getElementById('cont').classList.toggle("ex")
+    document.getElementById('cont').classList.toggle("ex");
+    document.getElementById('container').classList.toggle("containerActive");
 }
