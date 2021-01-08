23ec4c165c8ed205e5a6d1df637788b3e2f4ca68
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-01-08 20:37:28 -0300

    feat(weather): add basic informations

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [location, setLocation] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    });
+  }, []);
+
+  if (location) {
+    return (
+      <>
+        <h3>Clima nas suas Coordenadas</h3>
+        <hr />
+        <ul>
+          <li>Temperatura atual: xº</li>
+          <li>Temperatura máxima: xº</li>
+          <li>Temperatura minima: xº</li>
+          <li>Pressão: x hpa</li>
+          <li>Umidade: x%</li>
+        </ul>
+      </>
+    );
+  } else {
+    return <>Você precisa habilitar a localização no browser</>;
+  }
 }
 
 export default App;
