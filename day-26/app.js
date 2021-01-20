669c6d3c45eff438cc8cdcb556e359b8ebd72c5a
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-01-20 00:37:33 -0300

    feature(day-27): add js

--- day-26/app.js
+++ day-26/app.js
@@ -1,34 +1,41 @@
-const calculator = document.getElementsByClassName(‘.calculator’);
-const keys = document.getElementsByClassName(‘.keys’);
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelector('.keys');
+const display = document.querySelector('.display');
 
-console.log(calculator);
-
-keys.addEventListener(‘click’, e => {
- if (e.target.matches(‘button’)) {
-    const key = e.target
-    const action = key.dataset.action
+keys.addEventListener('click',(e) => {
+  if(e.target.matches('button')) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
     
-    if (!action) {
-        console.log('number key!')
-      } else if (
-        action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'
-      ) {
-        console.log('operator key!')
+    if(!action) {
+      if(displayedNum === '0') {
+          display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
       }
+    } 
+    
+    if(
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      console.log('operador');
+    } 
+    
+    if(action === 'decimal') {
 
-      if (action === 'decimal') {
-        console.log('decimal key!')
-      }
-      
-      if (action === 'clear') {
-        console.log('clear key!')
-      }
-      
-      if (action === 'calculate') {
-        console.log('equal key!')
-      }
- }
+    } 
+    
+    if(action === 'clear') {
+      console.log('clear')
+    } 
+    
+    if(action === 'calculate') {
+      console.log('equal')
+    }
+  }
 })
